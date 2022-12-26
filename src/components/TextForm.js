5ea5107d88f590496c5ba1f4331ae4186df9cf15
sahildhimandesigner <sahil@founderandlightning.com>
2022-12-26 16:01:18 +0530

    added the word and charcter count functionality

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,12 +15,20 @@ export default function TextForm(props) {
     }
 
   return (
-    <div>        
-        <h4>{props.heading}</h4>
-        <div className="mb-3">            
-            <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+    <>
+        <div>        
+            <h4>{props.heading}</h4>
+            <div className="mb-3">            
+                <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+            </div>
+            <button onClick={handleUpClick} className="btn btn-primary">Convert to Uppercase</button>
         </div>
-        <button onClick={handleUpClick} className="btn btn-primary">Convert to Uppercase</button>
-    </div>
+        <div className='container my-4'>
+            <h5> {text.split(" ").length} words and {text.length}: characters</h5>
+            <h5>Reading Time: {0.008 * text.split(" ").length}</h5>
+        </div>
+        <h5>Preview:</h5>
+        <p>{text}</p>
+    </>
   )
 }
