42e0c33edd7025fb3c345ffebdb2787204487ee2
sahildhimandesigner <sahil@founderandlightning.com>
2023-01-08 17:19:06 +0530

    created the new component for show alert message

--- src/App.js
+++ src/App.js
@@ -1,31 +1,43 @@
 import React, { useState } from "react";
 import About from "./components/About";
+import Alert from "./components/Alert";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 
 function App() {
 
   const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+      setAlert({
+        msg: message,
+        type: type
+     })
+  }
 
   const toggleMode = () => {
     if(mode === 'light') {
       setMode('dark');
       document.body.style.backgroundColor = '#212529';
       document.body.style.color = '#FFFFFF';
+      showAlert('Dark mode has been enable', 'success');
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = '#FFFFFF';
       document.body.style.color = '#000000';
+      showAlert('Light mode has been enable', 'success');
     }
   }
 
   return (
     <div className="App">
       <Navbar title="TextUtils 2" aboutText="About text" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
         <TextForm heading="Enter the text to Analyze" />
-        <About />
+        {/* <About /> */}
       </div>      
     </div>
   );
